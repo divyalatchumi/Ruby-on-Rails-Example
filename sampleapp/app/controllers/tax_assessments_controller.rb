778dbacc0bbb435f602cdb46079f7da0ea4839bb
Divyalatchumi Nakkeerar <divyalatchumi@gmail.com>
2018-12-20 11:18:30 +0530

    Tax Assessment Assignment - Create, edit and delete tax assessments

--- sampleapp/app/controllers/tax_assessments_controller.rb
+++ sampleapp/app/controllers/tax_assessments_controller.rb
@@ -13,27 +13,19 @@ class TaxAssessmentsController < ApplicationController
 	end
 
 	def create
-
 	end
 
 	def edit
 	end
 
 	def update
-		if params[:Itr_type].nil? || params[:profit].nil? || params[:revenue].nil? || params[:assessment_year].nil? 
-			render :status => :not_implemented, :json =>{:message => 'Fields cannot be empty'}
-		
-		elsif params[:AckNo].to_s.length<15
-			render :status => :not_implemented, :json => {:message => 'Acknowledgement number must have 15 digits'}
-		else
-			@taxAssessment = TaxAssessment.find(params[:id])
-			if @taxAssessment.update_attributes(:Itr_type => params[:Itr_type], 
-													:AckNo => params[:AckNo],
-													:profit => params[:profit],
-													:revenue => params[:revenue],
-													:assessment_year => params[:assessment_year])
-				render :status => :ok
-			end
+		@taxAssessment = TaxAssessment.find(params[:id])
+		if @taxAssessment.update_attributes(:Itr_type => params[:Itr_type], 
+												:AckNo => params[:AckNo],
+												:profit => params[:profit],
+												:revenue => params[:revenue],
+												:assessment_year => params[:assessment_year])
+			render :status => :ok
 		end
 	end
 
@@ -43,36 +35,24 @@ class TaxAssessmentsController < ApplicationController
 	end
 
 	def getItrTypes
-		render :status => :ok, :json => {:Itr_types => ["Itr-4", "Itr-4s", "Itr-6", "Itr-6s", "Itr-8", "Others"]}
+		render :status => :ok, :json => {:Itr_types => TaxAssessment.getItrTypes()}
 	end
 
 	def getAssessmentYears
-		current_year = Time.current.year
-		assessmentYears = []
-		for yy in ((current_year-4)..(current_year-1))
-			assessmentYears.push({:value => yy, :text => yy.to_s + " - " + (yy+1).to_s})
-		end
-		render :status => :ok, :json => {:assessmentYears => assessmentYears}
+		render :status => :ok, :json => {:assessmentYears => TaxAssessment.getAssessmentYears()}
 	end
 
 	def taxAssessmentRecord
-		if params[:Itr_type].nil? || params[:profit].nil? || params[:revenue].nil? || params[:assessment_year].nil? 
-			render :status => :not_implemented, :json =>{:message => 'Fields cannot be empty'}
-		
-		elsif params[:ack_no].nil?
-			render :status => :not_implemented, :json => {:message => 'Acknowledgement number must have 15 digits'}
-		else	
-			@taxAssessment = TaxAssessment.new(:Itr_type => params[:Itr_type], 
-												:AckNo => params[:ack_no],
-												:profit => params[:profit],
-												:revenue => params[:revenue],
-												:assessment_year => params[:assessment_year],
-												:logins_id => @current_user )
-			if @taxAssessment.save
-				render :status => :ok
-			else
-				render :status => :not_implemented
-			end
+		@taxAssessment = TaxAssessment.new(:Itr_type => params[:Itr_type], 
+											:AckNo => params[:ack_no],
+											:profit => params[:profit],
+											:revenue => params[:revenue],
+											:assessment_year => params[:assessment_year],
+											:logins_id => @current_user )
+		if @taxAssessment.save
+			render :status => :ok
+		else
+			render :status => :not_implemented
 		end
 	end
 
--- sampleapp/app/models/tax_assessment.rb
+++ sampleapp/app/models/tax_assessment.rb
@@ -3,4 +3,19 @@ class TaxAssessment < ActiveRecord::Base
 	#validates_length_of :AckNo, :is => 15
 	validates_uniqueness_of :AckNo
 	validates_as_itr_type :Itr_type
+
+	@ItrTypes = ["Itr-4", "Itr-4s", "Itr-6", "Itr-6s", "Itr-8", "Others"]
+
+	def self.getAssessmentYears
+		current_year = Time.current.year
+		assessmentYears = []
+		for yy in ((current_year-4)..(current_year-1))
+			assessmentYears.push({:value => yy, :text => yy.to_s + " - " + (yy+1).to_s})
+		end
+		return assessmentYears
+	end
+
+	def self.getItrTypes
+		return @ItrTypes
+	end
 end
