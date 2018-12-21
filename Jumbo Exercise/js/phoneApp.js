5073f799332c5e7f9cdf8f4c8d3ed843f3762a41
Divyalatchumi Nakkeerar <divyalatchumi@gmail.com>
2018-12-21 15:40:10 +0530

    Jumbo Exercise Update

--- Jumbo Exercise/js/phoneApp.js
+++ Jumbo Exercise/js/phoneApp.js
@@ -3,24 +3,24 @@ app.controller('phoneController',['$scope', function($scope){
 	$scope.phones = [
 		{
 		   id: 1,
-		   name: "HONOR 8X 128GB 4G DUAL SIM, red ",
-		   icon: "honor_8x.jpg",
+		   name: "HONOR 8X 128GB 4G DUAL SIM",
+		   icon: "img/honor_8x.jpg",
 		   brand: "HONOR",
 		   variant: "Red",
 		   price: 19047
 		},
 		{
 		   id: 2,
-		   name: "APPLE IPHONE XR, coral, 256gb",
-		   icon: "iphone_xr.jpg",
+		   name: "APPLE IPHONE XR, 256gb",
+		   icon: "img/iphone_xr.jpg",
 		   brand: "APPLE",
 		   variant: "Coral",
 		   price: 68800
 		},
 		{
 		   id: 3,
-		   name: "HUAWEI P20 PRO 128GB DUAL SIM,  purple",
-		   icon: "huwai_p20.jpg",
+		   name: "HUAWEI P20 PRO 128GB DUAL SIM",
+		   icon: "img/huwai_p20.jpg",
 		   brand: "HUAWEI",
 		   variant: "Purple",
 		   price: 41915
